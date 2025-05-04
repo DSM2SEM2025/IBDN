dcefbbeaa4c1a75d24d7d7825062eadb60d815e7
DaviBMachado <davimachado91210@gmail.com>
2025-05-04 19:40:43 -0300

    rota post adcionar_empresa()

--- app/routers/routes_empresa.py
+++ app/routers/routes_empresa.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 from typing import List
-from app.models.schemas import Empresa
-from app.controllers.controller_empresa import get_empresas
+from app.models.schemas import Empresa, EmpresaCreate
+from app.controllers.controller_empresa import get_empresas, criar_empresas
 
 router = APIRouter()
 
 @router.get("/empresas", response_model=List[Empresa])
 def listar_empresas():
     return get_empresas()
+
+@router.post("/empresas")
+def adcionar_empresa(empresa: EmpresaCreate):
+    return criar_empresas
