1a9e42a16166182b5e1ee001904d0506feae22b4
DaviBMachado <davimachado91210@gmail.com>
2025-05-04 19:13:51 -0300

    classe EmpresaCreate

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
 from typing import Optional
 from datetime import datetime
 
@@ -14,3 +14,14 @@ class Empresa(BaseModel):
     site_empresas: Optional[str] = None
     data_cadastro: datetime
     ativo: bool
+
+class EmpresaCreate(BaseModel):
+    cnpj: constr(max_length=18)
+    razao_social: constr(max_length=50)
+    nome_fantasia: Optional[str] = None
+    usuario_id: int
+    telefone: Optional[str] = None
+    responsavel: Optional[str] = None
+    cargo_responsavel: Optional[str] = None
+    site_empresa: Optional[str] = None
+    ativo: Optional[bool] = True
