2c15771b4f8d568be2c8bfb0442b8df35ef92645
Carlos Leon3l <carlos.leonel268@gmail.com>
2025-05-06 14:04:07 -0300

    modify: selo_controller & create: repository

--- app/controllers/controller_selo.py
+++ app/controllers/controller_selo.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from typing  import Optional 
 from fastapi import HTTPException
 from ..database.config import get_db_config 
+from ..repository.selos_repository import select_selo_empresa
 
 def get_db_connection():
     try:
@@ -87,45 +88,12 @@ async def get_selos_por_empresas(
             if connection and connection.is_connected():
                 connection.close()
 
+
 def retornar_empresas_com_selos_criados():
-    config = get_db_config()
-    connection = mysql.connector.connect(**config)
     try:
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("""SELECT 
-            s.id,
-            s.codigo_selo,
-            s.data_emissao,
-            s.data_expiracao,
-            s.status,
-            DATEDIFF(s.data_expiracao, CURDATE()) AS dias_para_expirar,
-            e.id AS id_empresa,
-            e.razao_social
-            FROM selo s
-            JOIN empresa e ON s.id_empresa = e.id 
-            """)
-        todos_selos = [
-            {
-                "id": colunm["id"],
-                "codigo_selo":colunm["codigo_selo"],
-                "data_emissao": colunm["data_emissao"],  
-                "data_expiracao": colunm["data_expiracao"],
-                "status": colunm["status"],
-                "dias_para_expirar": colunm["dias_para_expirar"],
-                "id_empresa": colunm["id_empresa"],
-                "razao_social": colunm["razao_social"]
-            }
-            for colunm in cursor.fetchall()
-        ]
-    except Error as e:
-            raise HTTPException(
-                status_code=500,
-                detail=f"Erro ao buscar selos: {str(e)}"
-            )
-    finally:
-        if cursor:
-            cursor.close()
-        if connection and connection.is_connected():
-            connection.close()
-    return todos_selos
-        
+        selos = select_selo_empresa()
+        return {"dados": selos}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
