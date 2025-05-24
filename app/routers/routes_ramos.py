c8a5a1e2e0d97b1f71d6a701f9beb87ad2535bb1
DaviBMachado <davimachado91210@gmail.com>
2025-05-23 23:58:31 -0300

    rotas ramos e empresa x ramos

---
+++ app/routers/routes_ramos.py
@@ -0,0 +1,29 @@
+
+from fastapi import APIRouter, Path
+from app.models.model_ramo import RamoBase, RamoCreate, RamoUpdate
+from app.controllers.controller_ramo import (
+    get_ramos, get_ramo_by_id, create_ramo, delete_ramo, update_ramo
+)
+
+router = APIRouter()
+
+# rotas ramos
+@router.get("/ramos", response_model=RamoBase)
+def listar_ramo():
+    return get_ramos()
+
+@router.get("/ramos/{ramo_id}", response_model=RamoBase)
+def obter_ramo(ramo_id:int = Path(..., gt=0)):
+    return get_ramo_by_id
+
+@router.post("/ramos",response_model=RamoBase)
+def criar_ramo(ramo: RamoCreate):
+    return create_ramo(ramo)
+
+@router.put("/ramos/{ramo_id}", response_model=RamoBase)
+def atualizar_ramo(ramo_id: int = Path(..., gt=0), ramo:RamoUpdate = None):
+    return update_ramo(ramo_id, ramo)
+
+@router.delete("/ramos/{ramo_id}", status_code=204)
+def deletar_ramo(ramo_id:int = Path(..., gt=0)):
+   return delete_ramo(ramo_id)
