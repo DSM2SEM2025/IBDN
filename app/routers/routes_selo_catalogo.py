df7bb581f7635cee51c45e5b271b414966c43f55
wellingtonspdev <dihhhd71@icloud.com>
2025-06-17 22:10:28 -0300

    limpando código 3.0

--- app/routers/routes_selo_catalogo.py
+++ app/routers/routes_selo_catalogo.py
@@ -7,16 +7,15 @@ from app.controllers.token import require_permission
 router = APIRouter(
     prefix="/selos-catalogo",
     tags=["Catálogo de Selos"],
-    dependencies=[Depends(require_permission("admin", "admin_master"))],
     responses={404: {"description": "Não encontrado"}},
 )
 
 
-@router.post("/", response_model=SeloInDB, status_code=status.HTTP_201_CREATED, summary="Cria um novo tipo de selo no catálogo")
+@router.post("/", response_model=SeloInDB, status_code=status.HTTP_201_CREATED, summary="Cria um novo tipo de selo no catálogo", dependencies=[Depends(require_permission( "admin", "admin_master"))])
 def criar_selo_no_catalogo(data: SeloCreate):
     return ctrl.criar_tipo_selo(data)
 
 
-@router.get("/", response_model=List[SeloInDB], summary="Lista todos os tipos de selo do catálogo")
+@router.get("/", response_model=List[SeloInDB], summary="Lista todos os tipos de selo do catálogo",  dependencies=[Depends(require_permission("empresa", "admin", "admin_master"))])
 def listar_selos_do_catalogo():
     return ctrl.listar_tipos_selo()
